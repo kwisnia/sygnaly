9aaed1e1695d3cc4019d944df6a19d33806e3ad4
Wojciech Slabosz <230000@edu.p.lodz.pl>
2022-03-07 14:33:55 +0100

    functions definitions done

---
+++ functions.py
@@ -0,0 +1,48 @@
+def generate():
+   pass
+
+def generate_gauss_noise():
+   pass
+
+def generate_uniform_noise():
+   pass
+
+def generate_sinusoidal_signal():
+   pass
+
+def generate_sinusoidal_signal_one_half_straight():
+   pass
+
+def generate_sinusoidal_signal_straight():
+   pass
+
+def generate_rectangle_signal():
+   pass
+
+def generate_rectangle_signal_symmetric():
+   pass
+
+def generate_triangle_signal():
+   pass
+
+def generate_unit_jump_signal():
+   pass
+
+def generate_unit_impulse():
+   pass
+
+def generate_impulse_noise():
+   pass
+
+
+# ▪ (S1) szum o rozkładzie jednostajnym
+# ▪ (S2) szum gaussowski
+# ▪ (S3) sygnał sinusoidalny
+# ▪ (S4) sygnał sinusoidalny wyprostowany jednopołówkowo
+# ▪ (S5) sygnał sinusoidalny wyprostowany dwupołówkowo
+# ▪ (S6) sygnał prostokątny
+# ▪ (S7) sygnał prostokątny symetryczny
+# ▪ (S8) sygnał trójkątny
+# ▪ (S9) skok jednostkowy
+# ▪ (S10) impuls jednostkowy
+# ▪ (S11) szum impulsowy
