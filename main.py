98458d26e551ac5c2e99cd7cab80cd8634b5da20
wslabosz <62252112+wslabosz@users.noreply.github.com>
2022-03-16 21:42:06 +0100

    implemented loading from file

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import functions as fn
 from Signal import Signal
 from plot import plot
 
+
 if __name__ == "__main__":
-    signal = Signal(
-        amplitude=5,
-        signal_start_time=0,
-        signal_duration=5,
-        frequency=1,
-        type_of_signal=fn.generate_triangle_signal,
-    )
-    signal.save_to_file()
+    newsignal = Signal(3.0, 0, 5, 50, fn.generate_gauss_noise)
+    plot(newsignal)
+    fn.save_to_file(newsignal)
+    signal = fn.load_from_file("gaussnoise.json")
     plot(signal)
--- plot.py
+++ plot.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 from Signal import Signal
 
-def plot(signal: Signal):
+def plot_continuous(signal: Signal):
    plt.plot(signal.samples, signal.values)
+   plt.show()
+
+def plot_discrete(signal: Signal):
+   plt.scatter(signal.samples, signal.values)
    plt.show()
