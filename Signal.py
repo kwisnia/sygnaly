b973a6c2e2a7f97db3b5be981bea991a22ad166c
kwisnia <230037@edu.p.lodz.pl>
2022-03-16 20:59:33 +0100

    Implementing signals

--- Signal.py
+++ Signal.py
@@ -1,22 +1,35 @@
 from typing import List, Callable
 import json
-class Signal:
-   def __init__(self, amplitude: float, signal_start_time: float, signal_duration: float, frequency: int, type_of_signal: Callable):
-      self.signal_start_time = signal_start_time
-      self.signal_duration = signal_duration
-      self.frequency = frequency
-      self.samples, self.values = type_of_signal(amplitude, signal_duration, frequency, signal_start_time)
-      self.name = type_of_signal.__name__
 
-   def save_to_file(self):
-      name = "".join([str(elem) for elem in self.name.split("_")[1:]]) + ".json"
-      output = open(name, "w")
-      signal_dict = {
-         "signal_start_time": self.signal_start_time,
-         "signal_duration": self.signal_duration,
-         "frequency": self.frequency,
-         "samples": self.samples,
-         "values": self.values,
-      }
-      json.dump(signal_dict, output, indent=6)
-      output.close()
+
+class Signal:
+    def __init__(
+        self,
+        amplitude: float,
+        signal_start_time: float,
+        signal_duration: float,
+        frequency: int,
+        type_of_signal: Callable,
+        fullfilment: float = None,
+    ):
+        self.signal_start_time = signal_start_time
+        self.signal_duration = signal_duration
+        self.frequency = frequency
+        self.samples, self.values = type_of_signal(
+            amplitude, signal_duration, frequency, signal_start_time, fullfilment
+        )
+        self.name = type_of_signal.__name__
+        self.fullfilment = fullfilment
+
+    def save_to_file(self):
+        name = "".join([str(elem) for elem in self.name.split("_")[1:]]) + ".json"
+        output = open(name, "w")
+        signal_dict = {
+            "signal_start_time": self.signal_start_time,
+            "signal_duration": self.signal_duration,
+            "frequency": self.frequency,
+            "samples": self.samples,
+            "values": self.values,
+        }
+        json.dump(signal_dict, output, indent=6)
+        output.close()
--- functions.py
+++ functions.py
@@ -1,65 +1,142 @@
 from random import sample
 import numpy as np
 
-FREQUENCY = 50
+SAMPLES = 1000
 
 
-def generate_gauss_noise(amplitude: float, length: float,  frequency: int = 0, start: float = 0):
+def generate_gauss_noise(
+    amplitude: float,
+    length: float,
+    frequency: int = 0,
+    start: float = 0,
+    fulfillment: float = 0,
+):
     generator = np.random.default_rng()
-    samples = list(np.arange(start, start + length, 1 / FREQUENCY))
+    samples = list(np.linspace(start, start + length, SAMPLES))
     values = list([generator.normal(0, 1.0) for _ in samples])
     return samples, values
 
 
-def generate_uniform_noise(amplitude: float, length: float,  frequency: int = 0, start: float = 0,
+def generate_uniform_noise(
+    amplitude: float,
+    length: float,
+    frequency: int = 0,
+    start: float = 0,
+    fulfillment: float = 0,
 ):
     generator = np.random.default_rng()
-    samples = list(np.arange(start, start + length, 1 / FREQUENCY))
+    samples = list(np.linspace(start, start + length, SAMPLES))
     values = list([generator.uniform(-amplitude, amplitude) for _ in samples])
     return samples, values
 
 
-def generate_sinusoidal_signal(amplitude: float, length: float, frequency: int, start: float = 0, 
+def generate_sinusoidal_signal(
+    amplitude: float,
+    length: float,
+    frequency: int,
+    start: float = 0,
+    fulfillment: float = 0,
 ):
-    samples = list(np.arange(start, start + length, 1 / FREQUENCY))
+    samples = list(np.linspace(start, start + length, SAMPLES))
     print(samples)
     values = list(
-        amplitude * np.sin(2 * np.pi * frequency / FREQUENCY * (sample - start))
+        amplitude * np.sin(2 * np.pi * frequency * (sample - start))
         for sample in samples
     )
     return samples, values
 
 
-def generate_sinusoidal_signal_one_half_straight(amplitude: float, length: float, frequency: int = 0, start: float = 0
+def generate_sinusoidal_signal_one_half_straight(
+    amplitude: float,
+    length: float,
+    frequency: int = 0,
+    start: float = 0,
+    fulfillment: float = 0,
 ):
-    samples = list(np.arange(start, start + length, 1 / FREQUENCY))
+    samples = list(np.linspace(start, start + length, SAMPLES))
     values = list(
-        amplitude * np.sin(sample) if np.sin(sample) > 0 else 0 for sample in samples
+        amplitude * np.sin(2 * np.pi * frequency * (sample - start))
+        if amplitude * np.sin(2 * np.pi * frequency * (sample - start)) > 0
+        else 0
+        for sample in samples
     )
     return samples, values
 
 
-def generate_sinusoidal_signal_straight(amplitude: float, length: float, frequency: int = 0, start: float = 0
+def generate_sinusoidal_signal_straight(
+    amplitude: float,
+    length: float,
+    frequency: int = 0,
+    start: float = 0,
+    fulfillment: float = 0,
 ):
-    samples = list(np.arange(start, start + length, 1 / FREQUENCY))
-    values = list((abs(amplitude * np.sin(sample)) for sample in samples))
+    samples = list(np.linspace(start, start + length, SAMPLES))
+    values = list(
+        (
+            abs(amplitude * np.sin(2 * np.pi * frequency * (sample - start)))
+            for sample in samples
+        )
+    )
     return samples, values
 
 
-def generate_rectangle_signal():
-    pass
+def generate_rectangle_signal(
+    amplitude: float,
+    length: float,
+    frequency: int = 0,
+    start: float = 0,
+    fulfillment: float = 0,
+):
+    samples = list(np.linspace(start, start + length, SAMPLES))
+    values = list(
+        amplitude
+        if ((sample - start) * frequency) - np.floor((sample - start) * frequency)
+        < fulfillment
+        else 0
+        for sample in samples
+    )
+    return samples, values
 
 
-def generate_rectangle_signal_symmetric():
-    pass
+def generate_rectangle_signal_symmetric(
+    amplitude: float,
+    length: float,
+    frequency: int = 0,
+    start: float = 0,
+    fulfillment: float = 0,
+):
+    samples = list(np.linspace(start, start + length, SAMPLES))
+    values = list(
+        amplitude
+        if ((sample - start) * frequency) - np.floor((sample - start) * frequency)
+        < fulfillment
+        else -amplitude
+        for sample in samples
+    )
+    return samples, values
 
 
 def generate_triangle_signal():
     pass
 
 
-def generate_unit_jump_signal():
-    pass
+def generate_unit_jump_signal(
+    amplitude: float,
+    length: float,
+    frequency: int = 0,
+    start: float = 0,
+    fulfillment: float = 0,
+):
+    samples = list(np.linspace(start, start + length, SAMPLES))
+    values = list(
+        amplitude
+        if sample > fulfillment
+        else 0.5 * amplitude
+        if sample == fulfillment
+        else 0
+        for sample in samples
+    )
+    return samples, values
 
 
 def generate_unit_impulse():
