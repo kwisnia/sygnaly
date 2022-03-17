ad24c2c3e957c4d0d8cd546887fcfffe441455ed
Wojciech Slabosz <230000@edu.p.lodz.pl>
2022-03-17 19:20:20 +0100

    operations done

---
+++ operations.py
@@ -0,0 +1,35 @@
+from Signal import Signal
+import numpy as np
+from copy import deepcopy
+
+
+def add(first: Signal, second: Signal):
+   if (len(first) == len(second)):
+      new_signal = deepcopy(first)
+      new_signal.values = np.add(first.values, second.values)
+      return new_signal
+   return False
+
+
+def subtract(first: Signal, second: Signal):
+   if (len(first) == len(second)):
+      new_signal = deepcopy(first)
+      new_signal.values = np.subtract(first.values, second.values)
+      return new_signal
+   return False
+
+
+def multiply(first: Signal, second: Signal):
+   if (len(first) == len(second)):
+      new_signal = deepcopy(first)
+      new_signal.values = np.multiply(first.values, second.values)
+      return new_signal
+   return False
+
+
+def divide(first: Signal, second: Signal):
+   if (len(first) == len(second)):
+      new_signal = deepcopy(first)
+      new_signal.values = np.divide(first.values, second.values)
+      return new_signal
+   return False
