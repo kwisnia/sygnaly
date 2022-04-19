bb2755376cd8669cd77eb7b3881ee3212ad6ced2
Wojciech Slabosz <230000@edu.p.lodz.pl>
2022-04-19 15:03:08 +0200

    MSE, SNR, PSNR, MD implemented

--- stats.py
+++ stats.py
@@ -1,26 +1,14 @@
 from Signal import Signal
 import numpy as np
 from functions import SAMPLES
-
+import math
 
 def avg_signal_value(signal: Signal):
-    # if (signal.frequency is not None):
-    #    num_of_samples_in_interval = (len(signal.samples) / signal.signal_duration) / signal.frequency
-    #    print(num_of_samples_in_interval)
-    #    unnecessary_samples = len(signal.samples) % num_of_samples_in_interval
-    #    print(unnecessary_samples)
-    #    valid_samples = signal.samples[:len(signal.samples) - int(unnecessary_samples)]
-    #    value = (
-    #        1 / (valid_samples[-1] - valid_samples[0] + 1)) * np.sum(signal.values[:len(signal.samples) - unnecessary_samples])
-    # else:
     value = (1 / (signal.samples[-1] - signal.samples[0])) * np.sum(signal.values) * signal.signal_duration / SAMPLES 
     return value
 
 
 def abs_avg_signal_value(signal: Signal):
-    # if (signal.frequency is not None):
-    #    pass
-    # else:
     value = (1 / (signal.samples[-1] - signal.samples[0])) * np.sum(
         np.absolute(signal.values)  
     ) * signal.signal_duration / SAMPLES
@@ -28,9 +16,6 @@ def abs_avg_signal_value(signal: Signal):
 
 
 def avg_signal_power(signal: Signal):
-    # if (signal.frequency is not None):
-    #    pass
-    # else:
     value = (1 / (signal.samples[-1] - signal.samples[0])) * np.sum(
         np.power(signal.values, 2)
     ) * signal.signal_duration / SAMPLES
@@ -38,9 +23,6 @@ def avg_signal_power(signal: Signal):
 
 
 def variance_signal_value(signal: Signal):
-    # if (signal.frequency is not None):
-    #    pass
-    # else:
     avg_signal_val = avg_signal_value(signal)
     value = (1 / (signal.samples[-1] - signal.samples[0])) * np.sum(
         np.power(np.subtract(signal.values, avg_signal_val), 2)
@@ -49,8 +31,21 @@ def variance_signal_value(signal: Signal):
 
 
 def rms_signal_value(signal: Signal):
-    # if (signal.frequency is not None):
-    #    pass
-    # else:
     value = np.sqrt(avg_signal_power(signal))
     return value
+
+
+def mean_square_error(original_signal, reconstructed_signal):
+    return sum([(original_signal.values[i] - reconstructed_signal.values[i]) ** 2 for i in range(len(original_signal.samples))]) / len(original_signal.values)
+
+
+def signal_to_noise_ratio(original_signal, reconstructed_signal):
+    return 10 * math.log(sum((original_signal.values ** 2) / sum([(original_signal.values[i] - reconstructed_signal.values[i]) ** 2 for i in range(len(original_signal.samples))])), 10)
+
+
+def peak_signal_to_noise_ratio(original_signal, reconstructed_signal):
+    return 10 * math.log(max(original_signal.values)/mean_square_error(original_signal, reconstructed_signal), 10)
+
+
+def maximum_difference(original_signal, reconstructed_signal):
+    return max([original_signal.values[i] - reconstructed_signal.values[i] for i in range(len(original_signal.samples))])
