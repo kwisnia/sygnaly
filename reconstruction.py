7f731e78cd143656f62a80d7b4e29310c6bac360
Wojciech Slabosz <230000@edu.p.lodz.pl>
2022-04-21 16:39:08 +0200

    fix

--- reconstruction.py
+++ reconstruction.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
 
-def zero_order_hold(original_signal, quantization_threshold):
-   plt.step(original_signal.samples, [original_signal.values[i] + quantization_threshold for i in range(len(original_signal.samples))])
+def zero_order_hold(original_signal):
+   plt.step(original_signal.samples, [original_signal.values[i] for i in range(len(original_signal.samples))])
    plt.show()
