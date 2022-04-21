5727d8d38ab33745ef0020eeb636c511dd915edc
Wojciech Slabosz <230000@edu.p.lodz.pl>
2022-04-21 17:00:19 +0200

    sinc implemented

--- reconstruction.py
+++ reconstruction.py
@@ -1,5 +1,17 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def zero_order_hold(original_signal):
    plt.step(original_signal.samples, [original_signal.values[i] for i in range(len(original_signal.samples))])
+   plt.show()
+
+
+def first_order_hold(original_signal):
+   plt.plot(original_signal.samples, [original_signal.values[i] for i in range(len(original_signal.samples))])
+   plt.show()
+
+
+def sinc(original_signal):
+   plt.plot(original_signal.samples, np.sinc(original_signal.values))
    plt.show()
