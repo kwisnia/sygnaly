758c69d0d44919ce148d3f82a23b5648a27b48f3
kwisnia <230037@edu.p.lodz.pl>
2022-05-18 14:44:56 +0200

    Operations done

--- operations.py
+++ operations.py
@@ -40,20 +40,40 @@ def divide(first: Signal, second: Signal):
 
 
 def convolve(first: Signal, second: Signal):
-    if (len(second.samples) > len(first.samples)):
+    if len(second.samples) > len(first.samples):
         first, second = second, first
+
     new_signal = deepcopy(first)
     new_signal.values = np.convolve(first.values, second.values)
-    new_samples = len(new_signal.values) - len(first.values)
-    new_signal.samples.extend(np.linspace(first.samples[-1], first.signal_start_time + first.signal_duration + (1 / first.sample_rate) * new_samples, len(new_signal.values) - new_samples))
+    new_samples = len(new_signal.values) - len(first.values) + 1
+    print(
+        np.linspace(
+            first.samples[-1],
+            first.samples[-1] + (1 / first.sample_rate) * new_samples,
+            new_samples,
+        )[1:]
+    )
+    new_signal.samples.extend(
+        np.linspace(
+            first.samples[-1],
+            first.samples[-1] + (1 / first.sample_rate) * new_samples,
+            new_samples,
+        )[1:]
+    )
     return new_signal
 
 
 def corelate(first: Signal, second: Signal):
-    if (len(second.samples) > len(first.samples)):
+    if len(second.samples) > len(first.samples):
         first, second = second, first
     new_signal = deepcopy(first)
     new_signal.values = np.convolve(first.values, second.values[::-1])
     new_samples = len(new_signal.values) - len(first.values)
-    new_signal.samples.extend(np.linspace(first.samples[-1], first.signal_start_time + first.signal_duration + (1 / first.sample_rate) * new_samples, len(new_signal.values) - new_samples))
+    new_signal.samples.extend(
+        np.linspace(
+            first.samples[-1],
+            first.samples[-1] + (1 / first.sample_rate) * new_samples,
+            new_samples,
+        )[1:]
+    )
     return new_signal
