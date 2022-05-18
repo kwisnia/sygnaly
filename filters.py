9f7115b63c071f281a6c3522d691f04841d0a514
kwisnia <230037@edu.p.lodz.pl>
2022-05-19 00:09:07 +0200

    Signal generations done

--- filters.py
+++ filters.py
@@ -3,7 +3,9 @@ import numpy as np
 from windows import blackman_window, rectangular_window
 
 
-def low_pass_filter(filter_order, cutoff_frequency, window_type, sample_rate):
+def low_pass_filter(
+    amplitude, filter_order, cutoff_frequency, start_time, window_type, sample_rate
+):
     if window_type == 1:
         window = [
             blackman_window(filter_order, sample_index)
@@ -13,7 +15,7 @@ def low_pass_filter(filter_order, cutoff_frequency, window_type, sample_rate):
         window = [rectangular_window() for _ in range(filter_order)]
     k = sample_rate / cutoff_frequency
     center_sample = (filter_order - 1) / 2
-    return np.linspace(0, 1 / sample_rate * filter_order), np.array(
+    return np.linspace(0, 1 / sample_rate * filter_order, filter_order), np.array(
         [
             2 / k
             if sample_index == center_sample
@@ -24,13 +26,20 @@ def low_pass_filter(filter_order, cutoff_frequency, window_type, sample_rate):
     ) * np.array(window)
 
 
-def high_pass_filter(filter_order, cutoff_frequency, window_type, sample_rate):
-    return np.linspace(0, 1 / sample_rate * filter_order), np.array(
+def high_pass_filter(
+    amplitude, filter_order, cutoff_frequency, start_time, window_type, sample_rate
+):
+    return np.linspace(0, 1 / sample_rate * filter_order, filter_order), np.array(
         [
             x * (-1) ** i
             for i, x in enumerate(
                 low_pass_filter(
-                    filter_order, cutoff_frequency, window_type, sample_rate
+                    amplitude,
+                    filter_order,
+                    cutoff_frequency,
+                    start_time,
+                    window_type,
+                    sample_rate,
                 )[1]
             )
         ]
