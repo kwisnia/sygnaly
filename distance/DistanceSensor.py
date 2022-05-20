a39f1cbb7d21762bb415e96b4bd7406c8a90d592
kwisnia <230037@edu.p.lodz.pl>
2022-05-20 20:18:45 +0200

    Distance done

--- distance/DistanceSensor.py
+++ distance/DistanceSensor.py
@@ -1,9 +1,13 @@
-import operator
+from cProfile import label
+from scipy.signal import square, sawtooth
 from SignalFactory import SignalFactory
-from functions import generate_rectangle_signal, generate_sinusoidal_signal, generate_triangle_signal
+from functions import (
+    generate_rectangle_signal,
+    generate_sinusoidal_signal,
+    generate_triangle_signal,
+)
 from operations import add, corelate
 import numpy as np
-
 
 class DistanceSensor:
     def __init__(
@@ -18,23 +22,34 @@ class DistanceSensor:
         self.signal_factory = SignalFactory()
 
     def generate_probing_signal(self, range_start, delay=0.0):
-        sig_len = (1 / self.sample_rate) * self.buffer_len
+        sig_len = self.buffer_len / self.sample_rate
         first_signal = self.signal_factory.create(
             1,
-            range_start + delay,
+            range_start,
             sig_len,
-            self.probe_signal_period,
+            1 / self.probe_signal_period,
             self.sample_rate,
             generate_sinusoidal_signal,
+            None,
+            range_start - delay,
         )
         second_signal = self.signal_factory.create(
-            0.6,
-            range_start + delay,
+            0.5,
+            range_start,
             sig_len,
-            self.probe_signal_period / 6 * 2,
+            1 / (self.probe_signal_period / 4 * 2),
             self.sample_rate,
-            generate_rectangle_signal,
-            0.3,
+            generate_triangle_signal,
+            0.25,
+            range_start - delay,
+        )
+        first_signal.values = np.sin(2 * np.pi * (1 / self.probe_signal_period) * (np.array(first_signal.samples) - delay))
+        second_signal.values = sawtooth(
+            2
+            * np.pi
+            * (1 / (self.probe_signal_period / 6 * 2))
+            * (np.array(second_signal.samples) - delay),
+            duty=0.3,
         )
         return add(first_signal, second_signal)
 
@@ -55,7 +70,7 @@ class DistanceSensor:
         self.correlated_signal = corelate(self.feedback_signal, self.probe_signal)
 
         index = np.argmax(
-            self.correlated_signal.values[: len(self.correlated_signal.values) // 2]
+            self.correlated_signal.values[len(self.correlated_signal.values) // 2:]
         )
 
         delay = index / self.sample_rate
